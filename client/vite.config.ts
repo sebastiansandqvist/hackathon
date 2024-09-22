bdf9e0c969980a6fa36ccba2afa61e9e344a199e
Sebastian Sandqvist <seb@sparque.me>
2024-09-22 10:10:51 -0700

    fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
   build: { minify: false },
   resolve: {
     alias: [
-      { find: '~/components', replacement: './src/components/index.ts' },
-      { find: '~/trpc', replacement: './src/trpc.ts' },
-      { find: '~/util', replacement: './src/util.ts' },
+      { find: '~/components', replacement: path.resolve(__dirname, './src/components/index.ts') },
+      { find: '~/trpc', replacement: path.resolve(__dirname, './src/trpc.ts') },
+      { find: '~/util', replacement: path.resolve(__dirname, './src/util.ts') },
     ],
   },
 });
